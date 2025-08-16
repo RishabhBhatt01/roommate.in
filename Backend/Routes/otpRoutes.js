22d96835d74f7bc9d39b5bb9c8c974ce9347593b
rishabh bhatt <rishabhbhatt088@gmail.com>
2025-08-16 16:53:04 +0530

    Routes for otp

--- Backend/Routes/otpRoutes.js
+++ Backend/Routes/otpRoutes.js
@@ -1,8 +1,11 @@
 import express from "express";
-import { sendOtp } from "../controllers/optControllers.js";
+import { sendOtp, verifyOtp } from "../controllers/otpControllers.js";
 
 const router = express.Router();
 
+console.log("otp routes are loaded");
+
 router.post("/send-otp",sendOtp); // "Hey Express, when someone sends a POST request to /send-otp, run the sendOtp function."
+router.post("/verify-otp",verifyOtp);
 
 export default router;
