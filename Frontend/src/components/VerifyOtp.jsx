09b5c45b9f5db55a6742427a1c11a86fa54ba5fe
rishabh bhatt <rishabhbhatt088@gmail.com>
2025-08-16 16:51:48 +0530

    otp verification  with basic ui

---
+++ Frontend/src/components/VerifyOtp.jsx
@@ -0,0 +1,64 @@
+import axios from "axios";
+import { useLocation } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
+import { useState } from "react";
+
+
+const VerifyOtp = () =>{
+  const location = useLocation();
+  const {sessionId} = location.state || {}; 
+  const [otp,setOtp] = useState("");
+  const [isVerifying,setIsVerifying] = useState(false);
+  const navigate = useNavigate();
+
+  if(!sessionId){
+    alert("No otp session found, Enter Mobile First");
+    navigate("/phone")
+  }
+
+  const handleSubmit = async(e) =>{
+    e.preventDefault();
+    setIsVerifying(true);
+    console.log("Otp submitted");
+    
+
+    try {
+      const response = await axios.post("http://localhost:5000/api/verify-otp",
+      {
+        otp,sessionId
+      },
+      );
+
+      // Checking if otp is verified or not
+      if(response.data.verified){
+        navigate("/password");
+      } else{
+        alert("otp verification failed")
+      }
+      console.log("otp verification data",response.data);
+    }catch(error){
+      alert("server error. Please try again later")
+      console.error("Error verification otp",error);
+      setIsVerifying(false);
+    }
+  }
+
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <label htmlFor="">Enter 6 digit otp</label>
+      <input type="text" 
+      value={otp}
+      pattern="\d{6}"
+      onChange={(e) => setOtp(e.target.value)}
+      placeholder="e.g 123456"
+      required
+      />
+      <button type="submit" disabled={isVerifying}>
+        {isVerifying ? "Verifying .." : "Verify"}      
+      verify</button>
+    </form>
+  )
+}
+
+export default VerifyOtp;
