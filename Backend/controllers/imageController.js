e2146a1af87bb6c64f674a252b95fdb9f892d69e
rishabh bhatt <rishabhbhatt088@gmail.com>
2025-08-17 20:56:25 +0530

    handles requests and responses

---
+++ Backend/controllers/imageController.js
@@ -0,0 +1,27 @@
+// Controller function: handles the response after upload
+import User from "../models/User.js"
+export const imageResponse = async(req, res) => {
+  try{
+    console.log("Uploaded file info : ",req.file);
+  if (!req.file) {
+    return res.status(400).json({ error: "No file uploaded" });
+  }
+
+  const {userId} = req.body;
+  const updatedUser = await User.findByIdAndUpdate(
+    userId,
+    {profilePicture : req.file.path},
+    {new:true}
+  )
+  console.log(updatedUser)
+
+  res.status(200).json({
+    message: "File uploaded successfully",
+    imageUrl: req.file.path,     // Cloudinary URL
+    publicId: req.file.filename, // needed for future deletion
+  });
+  }catch (error) {
+  console.error("Upload failed:", error);
+  res.status(500).json({ error: error.message || "upload failed" });
+}
+}
