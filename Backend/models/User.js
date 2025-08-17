2e9f41bd00aa485a5acd2cc53b240245de5bc715
rishabh bhatt <rishabhbhatt088@gmail.com>
2025-08-17 20:53:00 +0530

    user model updated, password field inserted along with profile picture

--- Backend/models/User.js
+++ Backend/models/User.js
@@ -1,24 +1,29 @@
 import mongoose from 'mongoose'
 const userSchema = new mongoose.Schema({
-  name : {
+  username : {
     type : String,
     required : true,
-  },
-  phone : {
-    type : String,
-    required : true,
-    unique : true,
   },
   role : {
     type : String,
     enum : ['tenant','owner'],
     required : true
   },
+    phone : {
+    type : String,
+    required : true,
+    unique : true,
+  },
+    password : {
+    type : String,
+    default : "",
+    required : false,
+  },
   profilePicture : {
     type : String,
     default : "",
   },                
-});
+}, {timestamps : true});
 
 const User = mongoose.model('User',userSchema);
 export default User;
