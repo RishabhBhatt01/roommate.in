6a5f40f33f78b97ec1753e5365fa21f68b239a56
rishabh bhatt <rishabhbhatt088@gmail.com>
2025-08-17 20:49:26 +0530

    middleware created for authenticating user using jwt

---
+++ Backend/middlewares/JWT.js
@@ -0,0 +1,19 @@
+import jwt from "jsonwebtoken";
+import {JWT_SECRET_KEY} from "../config/envConfig"
+
+
+export const requireAuth = (req,res,next) => {
+  const token = req.cookies.token;
+  if(!token)
+    return(
+  res.status(401).json({error : "Unauthoeized"})
+  )
+
+  try{
+    const decoded = jwt.verify(token,JWT_SECRET_KEY);// deode jwt
+    req.user = decoded;
+    next();
+  }catch(err){
+    return res.status(401).json({error : "Invalid token"});
+  }
+}
