07f8bb9e199cf1fc2645bc12f6fbd60781f76a54
RishabhBhatt01 <rishabhbhattpc@gmail.com>
2025-08-19 22:47:22 +0530

    bug solved, retuned token

--- Backend/utils/token.js
+++ Backend/utils/token.js
@@ -1,5 +1,7 @@
-const cookieToken = async (user) =>{
-  return token = jwt.sign(
+import jwt from "jsonwebtoken";            // to create the token
+import { JWT_SECRET_KEY } from "../config/envConfig.js";  //  secret key
+const cookieToken = (user) =>{
+  const token = jwt.sign(
         {
           id : user._id,
           role:user.role
@@ -8,7 +10,7 @@ const cookieToken = async (user) =>{
         {expiresIn : "7d"}
       );
 
+      return token;
+
 }
-
-
 export default cookieToken;
