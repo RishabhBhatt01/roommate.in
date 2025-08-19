bf9c5a74b12ab634c5e8a93e2c95d365da6bb6bd
RishabhBhatt01 <rishabhbhattpc@gmail.com>
2025-08-19 22:46:21 +0530

    User can now login.(minimal ui)

---
+++ Frontend/src/components/Login.jsx
@@ -0,0 +1,69 @@
+import axios from "axios";
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+
+const Login = () =>{
+  const [phone,setphone] = useState("");
+  const [password,setPassword] = useState("");
+  const [loggingin,setLoggingin] = useState(false);
+  const navigate = useNavigate();
+
+  const handleSubmit = async(e) => {
+    e.preventDefault();
+    console.log("Login credentials sent");
+
+    setLoggingin(true);
+
+    try{
+      const response = await axios.post(
+        "http://localhost:5000/api/login",
+        {phone,password},
+        {withCredentials : true},
+      )
+      console.log("server login  response : ",response.data);
+      navigate("/home");
+      setPassword("");
+      setphone("");
+      
+    }
+    catch(error){
+      if(error.response){
+        alert(error.response.data.message)
+      }
+      else{
+        console.log("error , something went wrong",error);
+      }
+      setLoggingin(false);
+    }
+  }
+
+  return(
+    <form onSubmit={handleSubmit}>
+      <label>Enter MOBILE phone : </label>
+      <input
+      type="text"
+      value={phone}
+      onChange={(e)=>setphone(e.target.value)}
+      placeholder="Enter phone number  here"
+      required
+      /> <br /><br />
+
+      <label>Enter Password : </label>
+      <input 
+      type="password" 
+      value={password}
+      onChange={(e)=> setPassword(e.target.value)}
+      placeholder="Enter your password"
+      required
+      />
+      <button type="submit" disabled={loggingin}>
+        {loggingin ? "logging in" : "login"}
+
+      </button>
+    </form>
+  )
+
+
+}
+
+export default Login;
