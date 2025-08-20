ee344b6865327441e2bff8df344fc2598ebafede
RishabhBhatt01 <rishabhbhattpc@gmail.com>
2025-08-21 00:39:22 +0530

    room model created

--- Backend/models/Room.js
+++ Backend/models/Room.js
@@ -8,8 +8,14 @@ const roomSchema = new mongoose.Schema({
     type : String,
     required : true,
   },
+  totalRooms : {
+    type : String,
+    required : true,
+  },
   price : {
     type : String,
     required : true,
   }
 })
+const Room = mongoose.model('Room',roomSchema);
+export default Room;
