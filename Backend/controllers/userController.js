999bc9d5b61106a3a95c036a7b06b7cf537c93ea
RishabhBhatt01 <rishabhbhattpc@gmail.com>
2025-08-21 00:40:06 +0530

    user controller created and updated

---
+++ Backend/controllers/userController.js
@@ -0,0 +1,38 @@
+import User from "../models/User"
+export const getUser = async(req,res) =>{
+  try{
+    const user = await User.find();
+    res.json(user);
+  }
+  catch(err){
+    res.status(500).json({error : "Something went wrong"});
+  }
+}
+
+export const setUser = async(req,res)=>{
+  try{
+    const {username,role,phone} = req.body;
+
+    // Finding existing user : 
+    const existingUser = await User.findOne({phone});
+
+    if(existingUser){
+      return res.status(400).json({error:"Phone number exists"});
+    }
+
+    const newUser = new User({
+      username,
+      role,
+      phone
+    })
+
+    await newUser.save();
+    res.status(201).json({
+      "message" : "User stored in Database"
+    })
+  }catch(error){
+    res.status(500).json({
+      error : "Failed to create the user",
+    })
+  }
+}
