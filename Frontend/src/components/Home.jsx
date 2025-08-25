7d5161df1baeb69cdccb579490a51d31f6ba072b
RishabhBhatt01 <rishabhbhattpc@gmail.com>
2025-08-25 21:34:37 +0530

    homepage updated

--- Frontend/src/components/Home.jsx
+++ Frontend/src/components/Home.jsx
@@ -1,18 +1,38 @@
+import axios from "axios";
+import { useState , useEffect } from "react";
 const Home = () => {
-  const handleSubmit = () => {
+  const [name,setName] = useState("");
+
+  useEffect(()=>{
+
+    const fetchUser = async()=>{
     try {
-      console.log("button clicked!");
+        const Response = await axios.get("http://localhost:5000/api/user",{
+        withCredentials : true,
+      })
+
+      console.log(Response.data.role);
+      console.log(Response.data.name)
+      setName(Response.data.name);
+
     } catch (error) {
-      console.log("chatgpt is noob");
+      if(error.response){
+        alert(error.response.data.error || "something fishy")
+      }
+      console.log(error);
     }
-  };
+  }
+    fetchUser();
+    },[])
 
   return (
     <>
-      <h1>Welcome to the homepage</h1>
+      <h1>Welcome , {name}</h1>
+
       <a href="/upload">upload profile picture</a>
       <br /><br />
       <a href="/owner">owner's tab</a>
+      
     </>
   );
 };
