a71068c3771490a8b0be046edbcd46ace575bc3d
RishabhBhatt01 <rishabhbhattpc@gmail.com>
2025-08-25 21:43:09 +0530

    token  updated, now storing in cookies of browser

--- Backend/utils/token.js
+++ Backend/utils/token.js
@@ -1,16 +1,24 @@
-import jwt from "jsonwebtoken";            // to create the token
-import { JWT_SECRET_KEY } from "../config/envConfig.js";  //  secret key
-const cookieToken = (user) =>{
+import jwt from "jsonwebtoken";
+import { JWT_SECRET_KEY } from "../config/envConfig.js";
+
+
+const cookieToken = (user) => {
+  // Create JWT payload with user information
+  const payload = {
+    _id: user._id,
+    role: user.role
+  };
+
+  // Sign the token with secret key and set expiration
   const token = jwt.sign(
-        {
-          id : user._id,
-          role:user.role
-        },
-        JWT_SECRET_KEY,
-        {expiresIn : "7d"}
-      );
+    payload,
+    JWT_SECRET_KEY,
+    { 
+      expiresIn: "7d",
+      issuer: "roommate.in"
+    }
+  );
+  return token;
+};
 
-      return token;
-
-}
 export default cookieToken;
