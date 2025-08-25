af11fa05a851744f07b4dc7b96cc7acdb6364a1a
RishabhBhatt01 <rishabhbhattpc@gmail.com>
2025-08-25 21:35:50 +0530

    userRoutes updated to get a single user

--- Backend/Routes/userRoutes.js
+++ Backend/Routes/userRoutes.js
@@ -1,8 +1,10 @@
-import { getUser ,setUser } from "../controllers/userController.js";
+import { getUsers ,setUser , getUser } from "../controllers/userController.js";
 import express from "express"
+import { requireAuth } from "../middlewares/JWT.js";
 
 const router = express.Router();
 
-router.get("/users",getUser);
+router.get("/users",getUsers);
+router.get("/user",requireAuth, getUser);
 router.post("/register-user",setUser);
 export default router;
