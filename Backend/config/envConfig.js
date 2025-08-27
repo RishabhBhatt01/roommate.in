1b6e83c069222416d86d040428a0f39fb3eeb8f2
RishabhBhatt01 <rishabhbhattpc@gmail.com>
2025-08-27 11:06:54 +0530

    google map's api integrated

--- Backend/config/envConfig.js
+++ Backend/config/envConfig.js
@@ -9,3 +9,7 @@ export const TWOFACTOR_API_KEY=process.env.TWOFACTOR_API_KEY
 
 // #JWT SECRET KEYS
 export const JWT_SECRET_KEY=process.env.JWT_SECRET_KEY
+
+// geocode api key
+export const GOOGLE_MAPS_API_KEY=process.env.GOOGLE_MAPS_API_KEY
+export const GEOCODING_SECRET=process.env.GEOCODING_SECRET
