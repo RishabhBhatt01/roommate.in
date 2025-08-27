9916d59a7a7cd86d7efc440a2dbb3f01bbb413ed
RishabhBhatt01 <rishabhbhattpc@gmail.com>
2025-08-28 00:32:39 +0530

    Owner Verification Updated

--- Frontend/src/components/Owner.jsx
+++ Frontend/src/components/Owner.jsx
@@ -5,10 +5,8 @@ import { useNavigate } from "react-router-dom";
 const Owner = () =>{
 
     const [submitting,setSubmitting] =useState(false);
-    const [address,setAddress] = useState("");
-    const [g_address,setg_address] = useState("");
-    const [ownerPin,setOwnerPin] = useState("")
     const [ownerEmail,setOwnerEmail] = useState("");
+    const [ownerAadhaar,setOwnerAadhaar] = useState("");
     const navigate = useNavigate();
 
 
@@ -18,7 +16,7 @@ const Owner = () =>{
   try{
     const response = await axios.post("http://localhost:5000/api/owner-info",
 
-        {address,g_address,ownerEmail,ownerPin},
+        {ownerEmail,ownerAadhaar},
         {withCredentials : true}
     )
     alert("form submitted sucessfully")
@@ -40,35 +38,11 @@ const Owner = () =>{
     <>
     <h1>Owner's Dashboard</h1>
     <form onSubmit={handleSubmit}>
-      <label htmlFor="address">Enter room's address and landmark</label>
-
-      <textarea
-        name="address"
-        id="address"
-        rows="3"
-        cols="50"
-        placeholder="Enter your address"
-        value={address} 
-        onChange={(e) => setAddress(e.target.value)}
-      />
-      <br /><br />
 
 
-    <label htmlFor="g_address">Paste room's Google Maps location</label>
-    <textarea
-      id="g_address"
-      name="g_map_address"
-      rows="2"
-      cols="50"
-      value={g_address}
-      placeholder="Google map address here"
-      onChange={(e) => setg_address(e.target.value)}
-    />
-
-      <br /><br />
 
 
-      <label htmlFor="ownerEmail">Enter email id : </label>
+      <label htmlFor="ownerEmail">Enter Email </label>
       <input 
       type="email"
       placeholder="Enter mail id"
@@ -79,21 +53,23 @@ const Owner = () =>{
       />
       <br /><br />
 
-
-
-      <label htmlFor="ownerPin">Enter your pin code</label><br/>
+      <label htmlFor="ownerAadhaar">Enter Aadhaar </label>
       <input 
-      type="text" 
-      placeholder="Enter Pin code"
-      id="ownerPin"
-      name="ownerPin"
-      value={ownerPin} 
-      onChange={(e) => setOwnerPin(e.target.value)}
+      type="text"
+      placeholder="12-digit aadhar number"
+      id="ownerAadhaar"
+      name="ownerAadhaar"
+      value={ownerAadhaar}
+      onChange={(e) => setOwnerAadhaar(e.target.value)}
+      />
+      <br /><br />
+      
 
 
-      /><br /><br />
+
+<br /><br />
       <button  disabled={submitting}> 
-        {submitting ? "submitting" : "submit"}
+        {submitting ? "verifying" : "verify and proceed"}
       </button>
     </form>
     </>
