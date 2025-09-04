d6f057d010c642d740c93aa88c24a8aeb5042435
RishabhBhatt01 <rishabhbhattpc@gmail.com>
2025-09-04 18:20:35 +0530

    update : now can see available roons in a range in console

--- Frontend/src/components/Home.jsx
+++ Frontend/src/components/Home.jsx
@@ -2,6 +2,7 @@ import axios from "axios";
 import { useState , useEffect } from "react";
 const Home = () => {
   const [name,setName] = useState("");
+  const [range,setRange] = useState("");
 
   useEffect(()=>{
 
@@ -11,7 +12,7 @@ const Home = () => {
         withCredentials : true,
       })
 
-      setName(Response.data.name + " role " + Response.data.role);
+      setName(Response.data.name);
 
     } catch (error) {
       if(error.response){
@@ -23,13 +24,57 @@ const Home = () => {
     fetchUser();
     },[])
 
+
+    const handleSubmit = async(e) =>{
+      e.preventDefault();
+      try{
+        console.log("sending file atleast");
+          const getNearRooms = await axios.get("http://localhost:5000/api/nearby-rooms",{
+            range,
+            withCredentials : true
+        })
+        console.log(getNearRooms.data);
+        alert("fetched rooms sucessfully");
+        
+
+      }catch(error){
+        if(error.response){
+          alert(error.response.data.error || "frontend error");
+        }
+        console.log(error);
+
+      }
+    }
+
   return (
     <>
       <h1>Welcome , {name}</h1>
 
       <a href="/upload">upload profile picture</a>
-      <br /><br />
-      <a href="/owner">owner's tab</a>
+      <a href="/owner" style={{marginLeft:"50%"}}>owner's tab</a> <br /><br />
+
+      <form onSubmit={handleSubmit}>
+        <label htmlFor="range">RANGE km</label>
+        <input type="number" 
+        placeholder="10"
+        id="range"
+        name="range"
+        value={range}
+        min={1}
+        onChange={(e) => setRange(e.target.value)}
+        />
+
+
+        <button type="submit">Get Rooms</button>
+
+
+
+      </form>
+
+
+
+
+      
       
     </>
   );
