88d7cbf758afd0ea4b218903339233c692d96441
RishabhBhatt01 <rishabhbhattpc@gmail.com>
2026-01-21 20:47:25 +0530

    image uploading function modified

--- Frontend/src/components/Roomimage.jsx
+++ Frontend/src/components/Roomimage.jsx
@@ -1,8 +1,51 @@
+import React,{useState} from "react";
+import axios from 'axios'
+
 const RoomImg = ()=> {
+
+  const [files, setFiles] = useState({
+  room1: null,
+  room2: null,
+  room3: null,
+  toilet: null,
+  bathroom: null,
+  balcony: null
+});
+
+
+const handleFileChange = (e) => {
+  setFiles(prev => ({
+    ...prev,
+    [e.target.id]: e.target.files[0]
+  }));
+};
+
+
+
   const handleSubmit=async()=>{
+    if(!files.room1 || !files.toilet) {
+  alert("Required images missing");
+  return;
+}
+
+    const formData = new FormData();
+    Object.entries(files).forEach(([key, file]) => {
+    if (file) formData.append(key, file);
+});
+
     try{
+      // send a post request to backend
+      
+      const response = await axios.post("http://localhost:5000/api/upload", formData,{
+        withCredentials : true
+      });
+
+      alert("Image uploaded successfully");
+      console.log("Server response : ",response.data);
 
     }catch(error){
+      console.error("Upload failed : ",error);
+      alert("Image upload failed");
 
     }
     
@@ -14,13 +57,17 @@ const RoomImg = ()=> {
     <input 
     type="file" 
     id="room1"
+    accept="image/*" onChange={handleFileChange}
     required
     />
     <br /><br />
+
+
     <label htmlFor="room2">Upload Room2 image(optional)</label>
     <input 
     type="file" 
     id="room2"
+    accept="image/*" onChange={handleFileChange}
     />
     <br /><br />
 
@@ -28,6 +75,7 @@ const RoomImg = ()=> {
     <input 
     type="file" 
     id="room3"
+    accept="image/*" onChange={handleFileChange}
     />
     <br /><br />
 
@@ -35,6 +83,7 @@ const RoomImg = ()=> {
     <input 
     type="file" 
     id="toilet"
+    accept="image/*" onChange={handleFileChange}
     required
     />
     <br /><br />
@@ -43,7 +92,7 @@ const RoomImg = ()=> {
     <input 
     type="file" 
     id="bathroom"
-    required
+    accept="image/*" onChange={handleFileChange}
     />
     <br /><br />
 
@@ -51,14 +100,11 @@ const RoomImg = ()=> {
     <input 
     type="file" 
     id="balcony"
-    required
+    accept="image/*" onChange={handleFileChange}
     />
     <br /><br />
 
-
-
-
-
+    <button type="button" onClick={handleSubmit}>UPLOAD</button>
     </>
   )
 }
