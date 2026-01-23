fab4b65f17be391dca4dbddcaf0b79457ea7556a
RishabhBhatt01 <rishabhbhattpc@gmail.com>
2026-01-23 16:59:19 +0530

    owner access enabled

--- Backend/Routes/dashboardRoutes.js
+++ Backend/Routes/dashboardRoutes.js
@@ -1,8 +1,9 @@
 import dashboardController from "../controllers/dashboardController.js";
 import express from "express";
 import { requireAuth } from "../middlewares/JWT.js";
+import requireOwner from "../middlewares/requireOwner.js";
 
 const router = express.Router();
 
-router.get("/dashboard/address",requireAuth,dashboardController);
+router.get("/dashboard/address",requireAuth,requireOwner,dashboardController);
 export default router;
