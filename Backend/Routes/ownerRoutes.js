4994cc1c7bd956261cdebe9efe0514c799f81306
RishabhBhatt01 <rishabhbhattpc@gmail.com>
2026-01-23 16:59:56 +0530

    owner access enabled

--- Backend/Routes/ownerRoutes.js
+++ Backend/Routes/ownerRoutes.js
@@ -1,8 +1,9 @@
 import ownerController from "../controllers/ownerController.js";
 import { requireAuth } from "../middlewares/JWT.js";
+import requireOwner from "../middlewares/requireOwner.js";
 import express from "express"
 
 const router = express.Router();
 
-router.post("/owner-info",requireAuth,ownerController);
+router.post("/owner-info",requireAuth,requireOwner,ownerController);
 export default router;
