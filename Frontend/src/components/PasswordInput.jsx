556c94f4b4335db62e9918b6b19fcb8bbd23021a
RishabhBhatt01 <rishabhbhattpc@gmail.com>
2026-02-01 20:58:29 +0530

    navigation changed

--- Frontend/src/components/PasswordInput.jsx
+++ Frontend/src/components/PasswordInput.jsx
@@ -23,7 +23,7 @@ const SendPassword = () => {
       alert("Password set Successfully");
       console.log("server response : ",response.data);
       setpassword("") // clear input after sending password #good practice
-      navigate("/owner")
+      navigate("/user-details")
     }catch(error){
       console.error("Error sending password",error);
       setsubmitting(false);
