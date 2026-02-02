645362b4670774b01491fe232c24240a2848b609
RishabhBhatt01 <rishabhbhattpc@gmail.com>
2026-02-02 22:56:01 +0530

    Implement GPS location feature in UserAddress component and refactor address input handling

--- Frontend/src/components/UserAddress.jsx
+++ Frontend/src/components/UserAddress.jsx
@@ -1,25 +1,33 @@
 import { useState } from "react"
 import axios from "axios";
-import { useNavigate } from "react-router-dom";
+import { useNavigate, Navigate} from "react-router-dom";
+import { useAuth } from "../context/authContext";
 
 const UserAddress = () =>{
-  const [building,setBuilding] = useState("");
+    const { refreshUser } = useAuth();
+      const navigate = useNavigate();
+
+  
   const [city,setCity] = useState("");
-  const [landmark,setLandmark] = useState("");
+  // const [landmark,setLandmark] = useState("");
   const [district,setDistrict] = useState("");
   const [state,setState] = useState("");
-  const navigate = useNavigate();
+  const [addressMode,setAddressMode] = useState("manual");
+  const [latitude,setLatitude] = useState("");
+  const [longitude,setLongitude] = useState("");
+  
+
 
   const handleSubmit = async(e) =>{
-    console.log("form submitting");
     e.preventDefault();
     try{
     const response = await axios.post("http://localhost:5000/api/user-address",
-      {building,city,landmark,district,state},
+      {city,district,state},
       {withCredentials : true}
     )
     alert("submitted successfully")
     console.log(response.data);
+    await refreshUser();
     navigate("/home");
   }catch(error){
     if(error.response){
@@ -29,28 +37,70 @@ const UserAddress = () =>{
   }
   }
 
-  return (
-    <form onSubmit={handleSubmit}>
+  const handleUseCurrrentLocation = () => {
 
-      <label htmlFor="building">Enter Your Building/House/flatnumber</label>
-      <input type="text" 
-      placeholder="Building and flatnumber"
-      id="building"
-      name="building"
-      value={building}
-      onChange={(e) => setBuilding(e.target.value)}
-      required      
-      /> <br /><br />
+    // Check browser support
+    if(!navigator.geolocation){
+      alert("Geolocation is not supported by this browser");
+    }
+
+    // Asking for current location
+    navigator.geolocation.getCurrentPosition(
+      (position) =>{
+        const lat = position.coords.latitude;
+        const lng = position.coords.longitude;
+
+        // saving coordinates in state
+        setLatitude(lat);
+        setLongitude(lng);
+
+        // Switching to GPS MODE
+        setAddressMode("gps");
+
+
+        // Verification
+        console.log("Latitude:", lat);
+        console.log("Longitude:", lng);
+
+        console.log("MODE:", addressMode, "LAT:", latitude, "LNG:", longitude);
+
+        
+
+
+
+      },
+      (error) =>{
+          alert("Unable to fetch location. Please allow permission.");
+          console.error(error);
+      },
+      {
+        enableHighAccuracy : true,
+        timeout : 10000,
+        maximumAge : 0,
+      }
+    );
+
+  }
+
+  return (
+    <>
+    <form onSubmit={handleSubmit}>
       <label htmlFor="city">Enter your city/village/town</label>
       <input type="text" 
       placeholder="eg. Pithoragarh"
       id="city"
       name="city"
       value={city}
-      onChange={(e) => setCity(e.target.value)}
+      onChange={(e) => {
+      if (addressMode === "gps") {
+        setAddressMode("manual");
+      }
+      setCity(e.target.value);
+}}
+      disabled={addressMode === "gps"}
       /><br /><br />
 
-      <label htmlFor="landmark">Enter a landmark</label>
+      {/* <label htmlFor="landmark">Enter a landmark</label>
       <input
       type="text"
       placeholder="eg. near apollo hospital"
@@ -58,14 +108,20 @@ const UserAddress = () =>{
       name="landmark"
       value={landmark}
       onChange={(e) => setLandmark(e.target.value)}
-      /><br /><br />
+      /><br /><br /> */}
 
       <label htmlFor="district">Enter district name</label>
       <input type="text" 
       placeholder="eg. Ghaziabad"
       id="district"
       value={district}
-      onChange={(e) => setDistrict(e.target.value)}
+      onChange={(e) => {
+      if (addressMode === "gps") {
+        setAddressMode("manual");
+      }
+      setDistrict(e.target.value);
+}}
+      disabled={addressMode === "gps"}
       required
       /><br /><br />
 
@@ -75,13 +131,29 @@ const UserAddress = () =>{
       id="state"
       name="state"
       value={state}
-      onChange={(e) => setState(e.target.value)}
+      onChange={(e) => {
+      if (addressMode === "gps") {
+        setAddressMode("manual");
+      }
+      setState(e.target.value);
+}}
+      disabled={addressMode === "gps"}
+
 
       /><br /><br />
 
 
-      <button type="submit">Find Rooms</button>
+      <button type="submit">Save and Next</button>
     </form>
+
+    <h2>Or you can try precise Location...</h2>
+    <button
+      type="button"
+      onClick={handleUseCurrrentLocation}
+      >
+      Use My current Location
+      </button>
+    </>
   )
 
 }
