7b81532a9dcc44ed3765855146e66c598df2e4aa
RishabhBhatt01 <rishabhbhattpc@gmail.com>
2026-02-02 22:53:16 +0530

    Refactor ownerController to improve error handling and remove debug logs

--- Backend/controllers/ownerController.js
+++ Backend/controllers/ownerController.js
@@ -4,12 +4,15 @@ import Owner from '../models/Owner.js';
 
 const ownerController = async(req,res) =>{
   try{
-    console.log("req.body:", req.body);
-    console.log("req.user:", req.user);
 
     const {ownerEmail,ownerAadhaar} = req.body;
 
     const userId = req.user._id;
+    if (req.user.isOwnerDetailsComplete) {
+  return res.status(400).json({
+    error: "Owner address already submitted"
+  });
+}
 
     if(!userId){
       return res.status(401).json({error : " User is not authorized "});
@@ -27,6 +30,13 @@ const ownerController = async(req,res) =>{
     if(role != "owner"){
       return res.status(401).json({error : "User is not an owner"});
     }
+
+    const isOwner = await Owner.findOne({userId});
+if (isOwner) {
+  return res.status(400).json({
+    error: "Owner details already submitted"
+  });
+}
 
     const newOwner = new Owner({
       userId : userId,
@@ -51,8 +61,8 @@ const ownerController = async(req,res) =>{
     res.status(200).json({message : "success"});
 
   }catch(error){
-    console.error("Detailed error",error)
-    return error;
+    console.error(error);
+    return res.status(500).json({ error: error.message || "Internal server error" });
   }
 }
 export default ownerController
