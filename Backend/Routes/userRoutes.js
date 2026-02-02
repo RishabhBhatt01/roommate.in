ea002b89b918d73c5b6f9fa5f1d38843da7d6116
RishabhBhatt01 <rishabhbhattpc@gmail.com>
2026-02-02 22:56:56 +0530

    Remove unused user routes and clean up imports in userRoutes.js

--- Backend/Routes/userRoutes.js
+++ Backend/Routes/userRoutes.js
@@ -1,10 +1,9 @@
-import { getUsers ,setUser , getUser } from "../controllers/userController.js";
+import { setUser } from "../controllers/userController.js";
 import express from "express"
 import { requireAuth } from "../middlewares/JWT.js";
 
 const router = express.Router();
 
-router.get("/users",getUsers);
-router.get("/user",requireAuth, getUser);
+
 router.post("/register-user",setUser);
 export default router;
