9756a9110adce60cc722285524ecb9dba1561705
RishabhBhatt01 <rishabhbhattpc@gmail.com>
2026-02-02 22:49:52 +0530

    New details to frontend

--- Backend/controllers/meController.js
+++ Backend/controllers/meController.js
@@ -1,7 +1,10 @@
 const me = (req, res) => {
   return res.status(200).json({
     id: req.user._id,
-    role: req.user.role
+    role: req.user.role,
+    isProfileComplete : req.user.isProfileComplete,
+    isOwnerDetailsComplete : req.user.isOwnerDetailsComplete,
+    isCreatedRoom : req.user.isCreatedRoom
   });
 };
 export default me;
