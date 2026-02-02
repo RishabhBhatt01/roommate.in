aa0c7e2c52478cf440ca3ed4faa89f6ab1b19cd9
RishabhBhatt01 <rishabhbhattpc@gmail.com>
2026-02-02 22:52:28 +0530

    Refactor JWT middleware to include user validation and improve error handling

--- Backend/middlewares/JWT.js
+++ Backend/middlewares/JWT.js
@@ -1,19 +1,26 @@
 import jwt from "jsonwebtoken";
-import {JWT_SECRET_KEY} from "../config/envConfig.js"
+import User from "../models/User.js";
+import { JWT_SECRET_KEY } from "../config/envConfig.js";
 
-
-export const requireAuth = (req,res,next) => {
+export const requireAuth = async (req, res, next) => {
   const token = req.cookies.token;
-  if(!token)
-    return(
-  res.status(401).json({error : "Unauthorized"})
-  )
 
-  try{
-    const decoded = jwt.verify(token,JWT_SECRET_KEY);// deode jwt
-    req.user = decoded;
-    next();
-  }catch(err){
-    return res.status(401).json({error : "Invalid token"});
+  if (!token) {
+    return res.status(401).json({ error: "Unauthorized" });
   }
-}
+
+  try {
+    const decoded = jwt.verify(token, JWT_SECRET_KEY);
+
+    const user = await User.findById(decoded._id);
+
+    if (!user) {
+      return res.status(401).json({ error: "User not found" });
+    }
+
+    req.user = user;
+    next();
+  } catch (err) {
+    return res.status(401).json({ error: "Invalid token" });
+  }
+};
