e158af852c807283c765e11e4c48b1c45d6a23b4
RishabhBhatt01 <rishabhbhattpc@gmail.com>
2026-02-03 22:42:41 +0530

    Add requireOwner middleware to room image upload route

--- Backend/Routes/roomImageRoutes.js
+++ Backend/Routes/roomImageRoutes.js
@@ -4,12 +4,14 @@ import { roomImageResponse } from "../controllers/roomImageController.js"
 import roomUpload from "../middlewares/roomUpload.js"
 import cloudinary from "../config/cloudinary.js"
 import { requireAuth } from "../middlewares/JWT.js"
+import requireOwner from "../middlewares/requireOwner.js"
 
 const router = express.Router();
 
 router.post(
   "/upload/room",
   requireAuth,
+  requireOwner,
   roomUpload.single("file"),
   roomImageResponse
 );
