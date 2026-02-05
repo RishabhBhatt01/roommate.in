edfcdf0fe5dc12cc4776da611c4c21add9486a7d
RishabhBhatt01 <rishabhbhattpc@gmail.com>
2026-02-05 22:44:20 +0530

    deleted file

--- Backend/Routes/addressRoutes.js
+++
@@ -1,9 +0,0 @@
-import express from "express"
-import {geocodeOwnerAddress} from "../controllers/geoCodeController.js";
-import { requireAuth } from "../middlewares/JWT.js";
-
-const router = express.Router();
-
-router.get("/addressCoordinates",requireAuth,geocodeOwnerAddress);
-
-export default router;
