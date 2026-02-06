56d9cd7df33a81d7f6f005ef660a5ed10866ca3d
RishabhBhatt01 <rishabhbhattpc@gmail.com>
2026-02-06 19:50:31 +0530

    Refactor googleAuthController to streamline Google OAuth2 authentication flow

--- Backend/controllers/googleAuthController.js
+++ Backend/controllers/googleAuthController.js
@@ -1,39 +1,30 @@
-import {OAuth2Client} from google-auth-library
-import GOOGLE_WEB_CLIENT_ID from '../config/envConfig'
+import User from '../models/User.js';
+import {OAuth2Client} from 'google-auth-library' // imported a library, which was installed by me.
+import {GOOGLE_WEB_CLIENT_ID , GOOGLE_WEB_CLIENT_SECRET} from '../config/envConfig.js'
+import cookieToken from '../utils/token.js';
 
-    const oauthClient = new OAuth2Client(GOOGLE_WEB_CLIENT_ID)
+    const oauthClient = new OAuth2Client(GOOGLE_WEB_CLIENT_ID,GOOGLE_WEB_CLIENT_SECRET,
+      "http://localhost:5000/api/auth/google/callback")
+
     if(!GOOGLE_WEB_CLIENT_ID){
       console.log("client id not found");
     }
-const googleAuth = async(req,res)=>{
-  try{
-     const idToken = req.body.idToken;
-     if(!idToken){
-      return res.status(400).json({Message : "idToken is required"})
-     }
 
-     const loginTicket = await oauthClient.verifyIdToken({idToken,audience : GOOGLE_WEB_CLIENT_ID});
+const googleAuth = (req, res) => {
+  const redirectUri = "http://localhost:5000/api/auth/google/callback";
 
-     const payload = loginTicket.getPayload();
-     if(payload == null || payload.email_verified !== true ){
-      return res.status(401).json({error : "Unauthorized"})
-     }
-     console.log(payload.name);
-     console.log(payload.email);
+const authUrl = oauthClient.generateAuthUrl({
+  access_type: "offline",
+  scope: [
+    "openid",
+    "email",
+    "profile",
+  ],
+  redirect_uri: redirectUri,
+});
 
 
-     res.status(200).json({messsage : "Successful"});
+  return res.redirect(authUrl);
+};
 
-
-
-
-  }
-  catch(error){
-    console.log(error);
-    return res.status(401).json({message : "Unauthorized"})
-
-  }
-  
-
-}
 export default googleAuth;
